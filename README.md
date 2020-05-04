4b424dac6af59b5e6a23b7b04babdf9a7710904d
Dmondragon413 <64608032+Dmondragon413@users.noreply.github.com>
2020-05-03 22:52:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Devcamp Js footer
+
+> This should be used in the following manner
+
+Install with the command:
+
+```
+npm install --save devacmp-js-footer
+```
+
+Add to a JavaScript project with the following code:
+
+```javascript
+import { footer } from 'devcamp-Js-footer';
+
+footer('some name')
+
+```
---
+++ index.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+var moment = require('moment');
+
+/**
+ * Returns a string element with a footer and updating year
+ * @param {string} name  //this means that the function expects a parameter, should be a type: string
+ * @return {string}
+ */
+//below we have EXPORTS, & it is an object and it is used to export packages.
+exports.footer = function (name) {
+    return "Copyright " + moment().format('YYYY') + " " + name + " All rights reserved";
+};
